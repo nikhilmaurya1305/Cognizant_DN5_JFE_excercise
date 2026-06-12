bfd8f24aad29a7ddce24622684ceb609ce4385fc
Nikhil Maurya <nikhilmaurya1305@gmail.com>
2026-06-12 09:36:15 +0530

    Builder Pattern Completed

---
+++ src/BuilderPattern/Computer.java
@@ -0,0 +1,46 @@
+package BuilderPattern;
+
+public class Computer {
+    private String CPU;
+    private int RAM;
+    private int Storage;
+
+    private Computer(Builder builder){
+        this.CPU = builder.CPU;
+        this.RAM = builder.RAM;
+        this.Storage = builder.Storage;
+    }
+
+    public void display(){
+        System.out.println("CPU: " + CPU);
+        System.out.println("RAM: " + RAM);
+        System.out.println("Storage: " + Storage);
+    }
+
+    public static class Builder{
+        private String CPU;
+        private int RAM;
+        private int Storage;
+
+        public Builder setCPU(String CPU){
+            this.CPU = CPU;
+            return this;
+        }
+        public Builder setRAM(int RAM){
+            this.RAM = RAM;
+            return this;
+        }
+        public Builder setStorage(int Storage){
+            this.Storage = Storage;
+            return this;
+        }
+
+        public Computer build(){
+            return new Computer(this);
+        }
+
+    }
+}
+
+
+

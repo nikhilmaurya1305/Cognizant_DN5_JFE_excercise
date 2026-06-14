6841788b6df6e308abb17cfe92cad070eeeff751
Nikhil Maurya <nikhilmaurya1305@gmail.com>
2026-06-14 10:04:45 +0530

    MVC Pattern Completed

---
+++ src/MVCPattern/StudentController.java
@@ -0,0 +1,23 @@
+package MVCPattern;
+
+public class StudentController {
+    private Student model;
+    private StudentView view;
+
+    public StudentController(Student model, StudentView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setName(String name){
+        model.setName(name);
+    }
+    public void setGrade(String grade){
+        model.setGrade(grade);
+    }
+
+    public void updateDetail(Student model,StudentView view){
+        view.displayStudentDetails(model.getName(), model.getId(), model.getGrade());
+    }
+
+}
---
+++ src/MVCPattern/StudentView.java
@@ -0,0 +1,10 @@
+package MVCPattern;
+
+public class StudentView {
+    public void displayStudentDetails(String name,int id,String grade){
+        System.out.println("Student Detail:");
+        System.out.println("Name: "+name);
+        System.out.println("ID: "+id);
+        System.out.println("Grade: "+grade);
+    }
+}

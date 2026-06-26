d1244c7654fa8d2c2b04b329bb50f039862ce364
Nikhil Maurya <nikhilmaurya1305@gmail.com>
2026-06-26 22:30:50 +0530

    BAsic spring core application completed

---
+++ src/SpringCoreAndMaven/LibraryManagement/src/main/java/org/example/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package org.example.repository;
+
+public class BookRepository {
+    public void displayBook() {
+        System.out.println("Book Repository Called");
+    }
+}
---
+++ src/SpringCoreAndMaven/LibraryManagement/src/main/java/org/example/service/BookService.java
@@ -0,0 +1,7 @@
+package org.example.service;
+
+public class BookService {
+    public void showService() {
+        System.out.println("Book Service Called");
+    }
+}
